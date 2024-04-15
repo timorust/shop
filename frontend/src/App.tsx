034db19b906800844218f9b1d0abb9235744451f
tamir.rust <timorust1@gmail.com>
2024-04-16 00:46:19 +0300

    add bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,44 @@
-// import { useState } from 'react'
-import './App.css'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { sampleProducts } from './data'
 
 function App() {
-	// const [count, setCount] = useState(0)
-
 	return (
-		<div>
-			<header>EL Shope</header>
+		<div className='d-flex flex-column vh-100'>
+			<header>
+				<Navbar bg='success' variant='success' expand='lg'>
+					<Container>
+						<Navbar.Brand>EL Shope</Navbar.Brand>
+					</Container>
+					<Nav>
+						<a href='/cart' className='nav-link'>
+							Cart
+						</a>
+						<a href='/signin' className='nav-link'>
+							Sign in
+						</a>
+					</Nav>
+				</Navbar>
+			</header>
 			<main>
-				<ul>
-					{sampleProducts.map(product => (
-						<li key={product.slug}>
-							<img src={product.image} alt='pic' className='product-image' />
-							<h2>{product.name}</h2>
-							<p>${product.price}</p>
-						</li>
-					))}
-				</ul>
+				<Container className='mt-3'>
+					<Row>
+						{sampleProducts.map(product => (
+							<Col sm={6} md={4} lg={3} key={product.slug}>
+								<img
+									src={product.image}
+									alt={product.name}
+									className='product-image'
+								/>
+								<h2>{product.name}</h2>
+								<p>${product.price}</p>
+							</Col>
+						))}
+					</Row>
+				</Container>
 			</main>
-			<footer>All rights reserved</footer>
+			<footer>
+				<div className='text-center'>All rights reserved</div>
+			</footer>
 		</div>
 	)
 }
