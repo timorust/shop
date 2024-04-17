9d4895f02e94aea200a63a91536babdb99395806
tamir.rustamov <timorust1@gmail.com>
2024-04-18 02:15:20 +0300

    product model

---
+++ backend/src/models/productModel.ts
@@ -0,0 +1,29 @@
+import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose'
+
+@modelOptions({ schemaOptions: { timestamps: true } })
+export class Product {
+	public _id!: string
+
+	@prop({ required: true })
+	public name!: string
+	@prop({ required: true, unique: true })
+	public slug!: string
+	@prop({ required: true })
+	public image!: string
+	@prop({ required: true })
+	public brand!: string
+	@prop({ required: true })
+	public category!: string
+	@prop({ required: true })
+	public description!: string
+	@prop({ required: true })
+	public price!: number
+	@prop({ required: true })
+	public countInStock!: number
+	@prop({ required: true })
+	public rating!: number
+	@prop({ required: true })
+	public numReviews!: number
+}
+
+export const ProductModel = getModelForClass(Product)
